50e7bd8163643210eaadc32468b2e0de341c8dbc
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2016-10-04 13:56:25 +0545

    Convert deeply nested hash keys to convert into string keys strip html tags inside values

--- lib/liquid_markdown/render_template.rb
+++ lib/liquid_markdown/render_template.rb
@@ -28,16 +28,43 @@ module LiquidMarkdown
 
       def liquidize
         t = Liquid::Template.parse(@template)
-        t.render(stringify_keys(@values), LIQUID_OPTIONS)
+        val = strip_html_values(@values)
+        val = deep_stringify_keys(val)
+        t.render(val, LIQUID_OPTIONS)
       end
+
+      private
 
       # convert key symbols to strings (required for converting values in liquidize render call)
       # Example:
       # {a: 'b'} => {'a' => 'b'}
-      def stringify_keys(hash)
-        new_hash = {}
-        hash.each {|k, v| new_hash[k.to_s] = v}
-        new_hash
+      def deep_stringify_keys(hash)
+        s2s = lambda do |h|
+                Hash === h ?
+                  Hash[
+                      h.map do |k, v|
+                        [k.respond_to?(:to_sym) ? k.to_s : k, s2s[v]]
+                      end
+                  ] : h
+              end
+        s2s[hash]
+      end
+
+      def strip_html_values(hash)
+        hash.each do |k, v|
+          if v.is_a?(String)
+            v.replace(strip_html(v))
+          elsif v.is_a?(Hash)
+            strip_html_values(v)
+          elsif v.is_a?(Array)
+            v.flatten.each { |x| strip_html_values(x) if x.is_a?(Hash) }
+          end
+        end
+      end
+
+      def strip_html(input)
+        empty = ''.freeze
+        input.to_s.gsub(/<script.*?<\/script>/m, empty).gsub(/<!--.*?-->/m, empty).gsub(/<style.*?<\/style>/m, empty).gsub(/<.*?>/m, empty)
       end
     end
   end
