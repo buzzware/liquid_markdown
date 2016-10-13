a7989cb3891c10a475257a9b6b9822d8dda3c8bf
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2016-10-13 15:36:20 +0545

    adding support for html and text templates

--- lib/liquid_markdown.rb
+++ lib/liquid_markdown.rb
@@ -6,6 +6,7 @@ require 'action_mailer'
 require 'liquid_markdown/strip'
 require 'liquid_markdown/keys'
 require 'liquid_markdown/template_handler'
+require 'liquid_markdown/resolver'
 
 module LiquidMarkdown
   class Render
@@ -58,8 +59,8 @@ module LiquidMarkdown
       LiquidMarkdown::Strip.strip_html_values(input_hash)
     end
   end
-
-  # let's use ActionMailer and ActionView for rendering mailer templates
-  # ActionMailer::Base.prepend_view_path(LiquidMarkdown::Render.new)
-  # ActionView::Template.register_template_handler(:mdt, ActionMailer::Markdown::TemplateHandler::Text)
 end
+
+ActionMailer::Base.prepend_view_path(LiquidMarkdown::Resolver.new)
+ActionView::Template.register_template_handler(:liqmd, LiquidMarkdown::TemplateHandler::HTML)
+ActionView::Template.register_template_handler(:liqmdt, LiquidMarkdown::TemplateHandler::Text)
--- lib/liquid_markdown/template_handler.rb
+++ lib/liquid_markdown/template_handler.rb
@@ -1,24 +1,55 @@
+# module LiquidMarkdown
+#   module TemplateHandler
+#     class Text
+#       def self.text_handler
+#         @@text_handler ||= ActionView::Template.register_template_handler(:liqmdt)
+#       end
+#
+#       def self.call(template)
+#         compiled_source = text_handler.call(template)
+#         "LiquidMarkdown::Render.new(begin;#{compiled_source};end)"
+#       end
+#     end
+#
+#     class HTML
+#       def self.html_handler
+#         @@_handler ||= ActionView::Template.register_template_handler(:liqmd)
+#       end
+#
+#       def self.call(template)
+#         compiled_source = html_handler.call(template)
+#         "LiquidMarkdown::Render.new(begin;#{compiled_source};end)"
+#       end
+#     end
+#   end
+# end
+
 module LiquidMarkdown
   module TemplateHandler
-    class Text
-      def self.erb_handler
-        @@erb_handler ||= ActionView::Template.register_template_handler(:erb)
-      end
+    UNDERSCORE = "_".freeze
+    OBJECT_ATTRIBUTE_MATCHER = /%\{([a-z0-9_]+\.[a-z0-9_]+)\}/i
 
+    def self.render(template, context, format)
+      puts context
+      binding.pry
+      variables = expand_variables(template, extract_variables(context))
+      source = template.rstrip % variables
+      ActionMailer::Markdown.public_send(format, source)
+    end
+
+    class Text
       def self.call(template)
-        compiled_source = erb_handler.call(template)
-        "LiquidMarkdown::Render.new(begin;#{compiled_source};end, {}, :text)"
+        %[
+          LiquidMarkdown::TemplateHandler.render.new(#{template.source.inspect}, self, :text)
+        ]
       end
     end
 
     class HTML
-      def self.erb_handler
-        @@erb_handler ||= ActionView::Template.register_template_handler(:erb)
-      end
-
       def self.call(template)
-        compiled_source = erb_handler.call(template)
-        "LiquidMarkdown::Render.new(begin;#{compiled_source};end, {}, :html)"
+        %[
+          LiquidMarkdown::TemplateHandler.render.new(#{template.source.inspect}, self, :html)
+        ]
       end
     end
   end
