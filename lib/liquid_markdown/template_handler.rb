4419f4d227abbc68d40596fdfaf0bb18561b2746
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2016-10-13 16:10:13 +0545

    adding support for html and text templates

--- lib/liquid_markdown/template_handler.rb
+++ lib/liquid_markdown/template_handler.rb
@@ -30,11 +30,27 @@ module LiquidMarkdown
     OBJECT_ATTRIBUTE_MATCHER = /%\{([a-z0-9_]+\.[a-z0-9_]+)\}/i
 
     def self.render(template, context, format)
-      puts context
-      binding.pry
       variables = expand_variables(template, extract_variables(context))
       source = template.rstrip % variables
       ActionMailer::Markdown.public_send(format, source)
+    end
+
+    def self.expand_variables(template, variables)
+      template.scan(OBJECT_ATTRIBUTE_MATCHER)
+          .map(&:first)
+          .each_with_object(variables) do |match, buffer|
+        target, attribute = match.split(".")
+        buffer[match.to_sym] = variables[target.to_sym].public_send(attribute)
+      end
+    end
+
+    def self.extract_variables(context)
+      context
+          .instance_variable_get(:@_assigns)
+          .each_with_object({}) do |(name, value), buffer|
+        next if name.start_with?(UNDERSCORE)
+        buffer[name.to_sym] = value
+      end
     end
 
     class Text
